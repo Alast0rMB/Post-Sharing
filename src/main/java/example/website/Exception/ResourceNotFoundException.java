d4306284b27af34b717b530001f33c062548487e
alast0r <alastorofploys@gmail.com>
2022-07-11 14:50:15 +0430

    added exception handler

---
+++ src/main/java/example/website/Exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package example.website.Exception;
+
+//Resource not found Exception
+public class ResourceNotFoundException extends RuntimeException {
+    private static final long serialVersionUID = 1L; //Used for checking the correct version
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
+}

c515340cb043ef6a1df413fc488c10e1ceab19fc
alast0r <alastorofploys@gmail.com>
2022-07-11 14:37:47 +0430

    added post services

--- src/main/java/example/website/Model/Post.java
+++ src/main/java/example/website/Model/Post.java
@@ -14,4 +14,33 @@ public class Post {
 
     @Column(length = 120)
     private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
 }
---
+++ src/main/java/example/website/Service/PostService.java
@@ -0,0 +1,43 @@
+package example.website.Service;
+
+import example.website.Model.Post;
+import example.website.Repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+    @Autowired
+    private PostRepository repository;
+
+    //Returns the list of all posts
+    public List<Post> getAll(){
+        return repository.findAll();
+    }
+
+    //Returns a post by its id
+    public Post getPost(Long id){
+        return repository.findById(id).orElseThrow(()->new RuntimeException());
+    }
+
+    //Adds a new post to the
+    public Post addPost(Post post){
+        return repository.save(post);
+    }
+
+    //Updates the new post based on given info
+    public Post updatePost(Long id, Post newPost){
+        Post post = getPost(id);
+        post.setDescription(newPost.getDescription());
+        post.setTitle(newPost.getTitle());
+        return repository.save(post);
+    }
+
+    //Delete a post from database
+    public void deletePost(Long id){
+        Post post = getPost(id);
+        repository.delete(post);
+    }
+}
